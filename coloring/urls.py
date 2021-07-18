e2bc4dbeac1aa018f4a37ed81bf0c28572e4b2a7
Dennis Mach <mach.dennis16@gmail.com>
2021-07-17 19:05:16 -0700

    Finished home page

--- coloring/urls.py
+++ coloring/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('demo', views.index, name='demo'),
+    path('home', views.home, name='home'),
     path('new_interaction', views.index, name='new_interaction')
 ]
